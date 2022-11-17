de4ae59bdbe6221b65786961bfaf34c5858bc18f
Raphael Dantas <105922938+dantasRaphael@users.noreply.github.com>
2022-11-17 09:28:57 -0300

    Update leitor.py

--- leitor.py
+++ leitor.py
@@ -3,11 +3,13 @@ import pytesseract
 
 caminho = 'C:/Users/NR Contabilidade/AppData/Local/Tesseract-OCR'
 
-img = Image.open("C:/Users/NR Contabilidade/Desktop/chamada.jpg")
+#img = Image.open("C:/Users/NR Contabilidade/Desktop/newPic.jpg")
+
+img = Image.open("C:/Users/NR Contabilidade/Documents/imagens/a1.jpeg")
+#
 
 pytesseract.pytesseract.tesseract_cmd = caminho + "/tesseract.exe"
 
 texto = pytesseract.image_to_string(img)
 print('a mensagem é: ')
 print(texto)
-
